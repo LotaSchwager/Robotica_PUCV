8995bc8a9066d291dd8b672d36eb557f423e5f10
LotaSchwager <ademir.munoz.r@mail.pucv.cl>
2026-04-01 13:02:28 -0300

    Switcher en las ruedas

--- laboratorio_1/controllers/Ruedas/Ruedas.py
+++ laboratorio_1/controllers/Ruedas/Ruedas.py
@@ -4,51 +4,52 @@
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot, Motor
 
-# create the Robot instance.
-robot = Robot()
-
-# get the time step of the current world.
-timestep = int(robot.getBasicTimeStep())
-
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getDevice('motorname')
-#  ds = robot.getDevice('dsname')
-#  ds.enable(timestep)
-
-left_motor = robot.getDevice("left wheel motor")
-right_motor = robot.getDevice("right wheel motor")
-
-left_motor.setPosition(float('inf'))
-right_motor.setPosition(float('inf'))
-
-left_motor.setVelocity(0.0)
-right_motor.setVelocity(0.0)
+def switcher(option):
+    match option:
+        # Línea rectal 
+        case 'Q1':
+            left_velocity = -0.5 * MAX_SPEED
+            right_velocity = 0.5 * MAX_SPEED
+            print("left_velocity: ", left_velocity)
+            print("right_velocity: ", right_velocity,"\n\n")
+            left_motor.setVelocity(left_velocity)
+            right_motor.setVelocity(right_velocity)
+            robot.step(1000)
+            left_motor.setVelocity(0)
+            right_motor.setVelocity(0)
+        # Girar en su eje    
+        case 'Q2':
+            left_velocity = -0.5 * MAX_SPEED
+            right_velocity = 0.5 * MAX_SPEED
+            print("left_velocity: ", left_velocity)
+            print("right_velocity: ", right_velocity, "\n\n")
+            left_motor.setVelocity(left_velocity)
+            right_motor.setVelocity(right_velocity)
+        # Girar hacia una dirección
+        case 'Q3':
+            left_velocity = 0.5 * MAX_SPEED
+            right_velocity = 0.25 * MAX_SPEED
+            print("left_velocity: ", left_velocity)
+            print("right_velocity: ", right_velocity, "\n\n")
+            left_motor.setVelocity(left_velocity)
+            right_motor.setVelocity(right_velocity)
+        # En caso de no calzar
+        case _:
+            print("Opción no válida\n\n")
 
 
 MAX_SPEED = 6.25
+robot = Robot()
+timestep = int(robot.getBasicTimeStep())
+left_motor = robot.getDevice("left wheel motor")
+right_motor = robot.getDevice("right wheel motor")
+left_motor.setPosition(float('inf'))
+right_motor.setPosition(float('inf'))
+left_motor.setVelocity(0.0)
+right_motor.setVelocity(0.0)
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-    
-    left_velocity = 0.5 * MAX_SPEED
-    right_velocity = 0.5 * MAX_SPEED
-    
-    print(left_velocity)
-    print(right_velocity)
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    
-    left_motor.setVelocity(left_velocity)
-    right_motor.setVelocity(right_velocity)
-    
+    switcher('Q1')
     pass
-
-# Enter here exit cleanup code.
